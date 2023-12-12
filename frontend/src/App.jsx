91cfffb53e786c1379acd6b03676ad8d59533fd5
Hanraf <raihanrafi96@gmail.com>
2023-12-12 15:42:41 +0700

    CORS testing

---
+++ frontend/src/App.jsx
@@ -0,0 +1,41 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+export default function App() {
+  const [question, setQuestion] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.post('http://localhost:8080/Api', {
+          umur: 18,
+          kelas: 11,
+          jenis_kelamin: "L",
+          id_pernyataan: ['C1', 'C7'],
+        });
+        setQuestion(response.data)
+      } catch (error) {
+        console.log(error)
+      } finally {
+        setIsLoading(false)
+      }
+    };
+    
+    fetchData();
+  }, []);
+
+  return (
+    isLoading !== true &&
+    <div className="w-full min-h-screen bg-slate-50">
+      <h1>Sorensen</h1>
+      {responseData && (
+      <pre>{JSON.stringify(responseData, null, 2)}</pre>
+      )}
+      {/* {
+        question.map((value, index) => {
+          return <p key={index}>{value}</p>
+        })
+      } */}
+    </div>
+  )
+}
